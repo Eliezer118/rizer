ea7fdf978d7a360a477e34901171a8f956347179
@SAYO <79429860+brenomakerr@users.noreply.github.com>
2024-06-12 09:31:49 -0300

    Update configure.js

--- configure.js
+++ configure.js
@@ -1,3 +1,3 @@
-global.coderNumero = ["5562936180708"];
+global.coderNumero = ["557481512604"];
 global.prefix = ["#"];
 global.conexaodb = "mongodb+srv://sayoz:34615194@cluster0.cdd0g.mongodb.net/elizerdb?retryWrites=true&w=majority";
